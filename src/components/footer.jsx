8c2d90ad3103b1176519c2724b7821acec1b9a22
Wesley Matthee <wrmatthee@gmail.com>
2023-02-07 12:23:55 -0500

    Adding footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import './footer.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse } from '@fortawesome/free-solid-svg-icons'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { faChartSimple } from '@fortawesome/free-solid-svg-icons'
+import { faPhone } from '@fortawesome/free-solid-svg-icons'
+import { useNavigate } from 'react-router-dom'
+
 
 export default function Footer(props){
-
+const navigate = useNavigate();
   return (
-    <footer className="footer">
-    <p>Testing footer</p>
-    </footer>
+
+      <footer className="footer">
+
+       <FontAwesomeIcon icon={faHouse} onClick={() => navigate("/")}/>
+       <FontAwesomeIcon icon={faChartSimple} />
+       <FontAwesomeIcon icon={faUser} />
+       <FontAwesomeIcon icon={faPhone} />
+      </footer>
+
   )
 }
