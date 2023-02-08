44eaf095761e7a77bf73c1ab83171a0ccb0f3e6c
Wesley Matthee <wrmatthee@gmail.com>
2023-02-08 09:11:10 -0500

    Import PhoneModal to Footer and useState

---
+++ src/components/PhoneModal.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export default function PhoneModal({ children }){
+
+  return(
+  <div>
+    {children}
+  </div>
+  )
+
+} 
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import './footer.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHouse } from '@fortawesome/free-solid-svg-icons'
@@ -6,20 +6,30 @@ import { faUser } from '@fortawesome/free-solid-svg-icons'
 import { faChartSimple } from '@fortawesome/free-solid-svg-icons'
 import { faPhone } from '@fortawesome/free-solid-svg-icons'
 import { useNavigate } from "react-router-dom";
+import PhoneModal from "./PhoneModal";
+import { useState } from 'react';
 
 
 export default function Footer(props){
-const navigate = useNavigate();
-  return (
+  const [isOpen, setIsOpen] = useState(false) 
+  const navigate = useNavigate();
 
+  return (
+    <>
       <footer className="footer">
        <div>
-       <FontAwesomeIcon icon={faHouse} onClick={() => navigate("/")} className="footer-icon"/>
-       <FontAwesomeIcon icon={faChartSimple} className="footer-icon"/>
-       <FontAwesomeIcon icon={faUser} className="footer-icon"/>
-       <FontAwesomeIcon icon={faPhone} className="footer-phone"/>
+         <FontAwesomeIcon icon={faHouse} onClick={() => navigate("/")} className="footer-icon"/>
+         <FontAwesomeIcon icon={faChartSimple} className="footer-icon"/>
+         <FontAwesomeIcon icon={faUser} className="footer-icon"/>
+         <FontAwesomeIcon icon={faPhone} onClick={ () => PhoneModal } className="footer-phone"/>
        </div>
       </footer>
+        <div>
+         <PhoneModal>
+            Call 911
+         </PhoneModal>
+        </div>
+    </>
 
   )
 }
