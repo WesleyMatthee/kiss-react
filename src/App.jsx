14a48b5381b7b38d95b0c944e028508bc1e5fec0
daejung90 <djung.ca@gmail.com>
2023-02-08 18:30:22 -0500

    dependencies fixing and downgrade react

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import React, { useState } from "react";
 
 import Nav from "./components/Templates/nav";
