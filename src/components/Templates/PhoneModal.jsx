a28b6c0c4930858b83e025314c084f81a280c102
Wesley Matthee <wrmatthee@gmail.com>
2023-02-08 21:00:44 -0500

    Added swipeable button - fix the message and set functions

--- src/components/Templates/PhoneModal.jsx
+++ src/components/Templates/PhoneModal.jsx
@@ -2,14 +2,17 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 import SwipeButton from 'react-swipezor';
-// import Swipeable from 'react-swipeable';
+import ReactSwipeButton from 'react-swipe-button';
 import  ReactDom  from 'react-dom';
 import React, { useState } from 'react';
 import './PhoneModal.css';
 
 
 export default function PhoneModal({ open, onClose }){
-  const [reset, setReset] = useState(0);
+  // const [reset, setReset] = useState(0);
+  const success = () => {
+  console.log("weell");
+  };
 
   if(!open) return null;
   
@@ -19,24 +22,16 @@ export default function PhoneModal({ open, onClose }){
       <div className='dark-overlay'>
         <div className='phone-modal'>
           <FontAwesomeIcon icon={faCircleXmark} onClick={onClose} className="close-modal"/>
-         <div className="swipe-button">        
-          <SwipeButton 
-            mainText="Call 911" 
-            overlayText="CALLING" 
-            onSwipeDone={function () {
-            console.log("Done!");
-           }} 
-            reset={reset}
-           />
-           </div> 
         </div>
-        <button onClick={() => {
-  setReset(counter => counter + 1)
-}}
->
-  Reset
-</button>
-       </div>
+        
+        <div className="swipe-button">
+          <ReactSwipeButton
+            text="CALL 911"
+            color="#f00"
+            onSuccess={success}
+          />
+        </div>
+      </div>
 
     </>,
     document.getElementById('portal')
