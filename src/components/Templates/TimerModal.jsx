97ee010b142677c4beaa3eb2a1b5d3846fab6b81
Wesley Matthee <wrmatthee@gmail.com>
2023-02-10 09:04:26 -0500

    Add CSS and fixed reset-btn

--- src/components/Templates/TimerModal.jsx
+++ src/components/Templates/TimerModal.jsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react';
 import './TimerModal.css';
 import ReactDom from 'react-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faRotateLeft } from '@fortawesome/free-solid-svg-icons';
 import './PhoneModal.css';
 
 export default function TimerModal({ open, onClose }) {
@@ -20,11 +21,11 @@ export default function TimerModal({ open, onClose }) {
 		}
 
 		let sec = 1000;
-		let seconds = sec * 5;
+		let seconds = sec * 15;
 		let setTime = seconds;
 		let startTime = Date.now();
 		let futureTime = startTime + setTime;
-    
+
 		function countDownTimer() {
 			if (semicircles.length > 0) {
 				const currentTime = Date.now();
@@ -62,24 +63,8 @@ export default function TimerModal({ open, onClose }) {
 	return ReactDom.createPortal(
 		<>
 			<div className='dark-overlay'>
-				<div className='close-modal'>
-					<FontAwesomeIcon
-						icon={faCircleXmark}
-						onClick={onClose}
-						className='close-modal'
-					/>
-				</div>
-				<h1 className='timer-numbers'>{Math.round(timer / 1000)}</h1>
-				{timer > 0 ? null : (
-					<button
-						onClick={() => {
-							setIsReset(!isReset);
-						}}
-					>
-						reset
-					</button>
-				)}
 				<div className='main-container center'>
+					<h1 className='timer-numbers'>{Math.round(timer / 1000)}</h1>
 					{/* progress indicator */}
 					<div className='circle-container center'>
 						<div className='semicircle'></div>
@@ -87,7 +72,21 @@ export default function TimerModal({ open, onClose }) {
 						<div className='semicircle'></div>
 						<div className='outermost-circle'></div>
 					</div>
-					{/* timer */}
+				</div>
+				<div className='timer-icons'>
+					<FontAwesomeIcon
+						icon={faXmark}
+						className='timer-icon'
+						onClick={onClose}
+					/>
+
+					<FontAwesomeIcon
+						icon={faRotateLeft}
+						className='timer-icon'
+						onClick={() => {
+							setIsReset(!isReset);
+						}}
+					/>
 				</div>
 			</div>
 		</>,
