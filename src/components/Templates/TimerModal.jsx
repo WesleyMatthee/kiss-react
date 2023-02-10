2b27e0259bbfb2ab3badd317b5713747c556b793
Wesley Matthee <wrmatthee@gmail.com>
2023-02-09 19:16:41 -0500

    Add timer-countdown loop and seconds

--- src/components/Templates/TimerModal.jsx
+++ src/components/Templates/TimerModal.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './TimerModal.css';
 import ReactDom from 'react-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -6,45 +6,53 @@ import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 import './PhoneModal.css';
 
 export default function TimerModal({ open, onClose }) {
+	const [timer, setTimer] = useState(15);
+	useEffect(() => {
+		const semicircles = document.querySelectorAll('.semicircle');
+		console.log(semicircles);
+
+		function countDownTimer() {
+			if (semicircles.length > 0) {
+				const currentTime = Date.now();
+				const remainingTime = futureTime - currentTime;
+				setTimer(remainingTime);
+				const angle = (remainingTime / setTime) * 360;
+				//progress indicator
+				if (angle > 180) {
+					semicircles[2].style.display = 'none';
+					semicircles[0].style.transform = 'rotate(180deg)';
+					semicircles[1].style.transform = `rotate(${angle}deg)`;
+				} else {
+					semicircles[2].style.display = 'block';
+					semicircles[0].style.transform = `rotate(${angle}deg)`;
+					semicircles[1].style.transform = `rotate(${angle}deg)`;
+				}
+				//timer
+				//5 second condition
+				if (remainingTime < 0) {
+					setTimer(false);
+					semicircles[0].style.display = 'none';
+					semicircles[1].style.display = 'none';
+					semicircles[2].style.display = 'none';
+				}
+			}
+			//end
+		}
+		const timerLoop = setInterval(countDownTimer);
+		return () => {
+			clearInterval(timerLoop);
+		};
+	}, [open]);
+
 	if (!open) return null;
 
 	//Timer Logic
-	const semicircles = document.querySelectorAll('.semicircle');
-	const sec = 10;
-	const seconds = sec * 1000;
+
+	const sec = 1000;
+	const seconds = sec * 5;
 	const setTime = seconds;
 	const startTime = Date.now();
 	const futureTime = startTime + setTime;
-
-	// const timerLoop = setInterval(countDownTimer);
-	// countDownTimer();
-
-	function countDownTimer() {
-		const currentTime = Date.now();
-		const remainingTime = futureTime - currentTime;
-		const angle = (remainingTime / setTime) * 360;
-		//progress indicator
-		if (angle > 180) {
-			semicircles[2].style.display = 'none';
-			semicircles[0].style.transform = 'rotate(180deg)';
-			semicircles[1].style.transform = `rotate(${angle}deg)`;
-		} else {
-			semicircles[2].style.display = 'block';
-			semicircles[0].style.transform = `rotate(${angle}deg)`;
-			semicircles[1].style.transform = `rotate(${angle}deg)`;
-		}
-		//timer
-		//5 second condition
-		if (remainingTime < 0) {
-			clearInterval(timerLoop);
-			semicircles[0].style.display = 'none';
-			semicircles[1].style.display = 'none';
-			semicircles[2].style.display = 'none';
-		}
-		//end
-	}
-
-	countDownTimer();
 
 	return ReactDom.createPortal(
 		<>
@@ -56,7 +64,8 @@ export default function TimerModal({ open, onClose }) {
 						className='close-modal'
 					/>
 				</div>
-				<h1 className='timer-numbers'>15</h1>
+				<h1 className='timer-numbers'>{Math.round(timer / 1000)}</h1>
+				{timer > 0 ? null : <button>reset</button>}
 				<div className='main-container center'>
 					{/* progress indicator */}
 					<div className='circle-container center'>
