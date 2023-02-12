ce4574d66830ec1554b8fff072742cb6afe8fee5
Wesley Matthee <wrmatthee@gmail.com>
2023-02-12 15:08:26 -0500

    Add nav logic and CSS

--- src/components/Templates/nav.jsx
+++ src/components/Templates/nav.jsx
@@ -1,25 +1,31 @@
-import './nav.css'
-import React from "react";
-import Timer from '../Timer' 
+import './nav.css';
+import React from 'react';
+import Timer from '../Timer';
 
-export default function Nav({isActive, seconds, setSeconds}) {
-
-    
-      return (
-        <nav>
-        <div className='nav-bar'>
-          <img alt='White Logo' className='white-logo' src='/images/Kiss-Logo-White.png'></img>
-          <h1 className='Kiss'>KISS</h1>
-        <output className='timer'>
-          {isActive && 
-          <Timer
-          isActive={isActive}
-          seconds={seconds}
-          setSeconds={setSeconds}
-          />}</output>
-        </div>
-    </nav>
-    )
-    
-  
+export default function Nav({ isActive, seconds, setSeconds }) {
+	return (
+		<nav>
+			<div className='nav-bar'>
+				<img
+					alt='White Logo'
+					className='white-logo'
+					src='/images/Kiss-Logo-White.png'
+				></img>
+				<img
+					alt='White Logo'
+					className='kiss'
+					src='/images/kiss_nav.png'
+				></img>
+				<output className='timer'>
+					{isActive && (
+						<Timer
+							isActive={isActive}
+							seconds={seconds}
+							setSeconds={setSeconds}
+						/>
+					)}
+				</output>
+			</div>
+		</nav>
+	);
 }
--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,21 +1,24 @@
-import React from "react";
-import { useStopwatch } from "react-timer-hook";
+import React from 'react';
+import { useStopwatch } from 'react-timer-hook';
 
 function MyStopwatch() {
-  const { seconds, minutes } = useStopwatch({ autoStart: true });
-  return (
-    <div style={{ textAlign: "center" }}>
-      <div style={{ fontSize: "15px" }}>
-        <span>{minutes}</span>:<span>{seconds}</span>
-      </div>
-    </div>
-  );
+	const { seconds, minutes } = useStopwatch({ autoStart: true });
+	const formatTime = time => {
+		return String(time).padStart(2, '0');
+	};
+	return (
+		<div style={{ textAlign: 'center' }}>
+			<div style={{ fontSize: '30px', fontFamily: 'Oswald' }}>
+				<span>{formatTime(minutes)}</span>:<span>{formatTime(seconds)}</span>
+			</div>
+		</div>
+	);
 }
 
 export default function Timer({ isActive, seconds, setSeconds }) {
-  return (
-    <div>
-      <MyStopwatch />
-    </div>
-  );
+	return (
+		<div>
+			<MyStopwatch />
+		</div>
+	);
 }
