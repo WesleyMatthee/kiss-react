6bc7e59e0b042cee6975e0471ef61351aa16c5a2
David Figueroa <figueroadavid93@gmail.com>
2023-02-13 14:52:28 -0500

    phone modal exit button enlarged and changed to white

--- src/components/subMonitor/ExtraNotes.jsx
+++ src/components/subMonitor/ExtraNotes.jsx
@@ -2,30 +2,31 @@ import '../../index.css';
 import React from 'react';
 
 export default function ExtraNotes(props) {
-	const { setSelectedMonitor, setIsHome, setIsPupils } = props;
+  const { setSelectedMonitor, setIsHome, setIsPupils } = props;
 
-	return (
-		<main className='Monitor'>
-			<section className='instructions'>
-				<ul className='instruction-steps'>
-					<p>Please input any extra information or notes here.</p>
-				</ul>
-			</section>
-			<form>
-      
-				<input type='text' className ='extraNotes-text'/>
-				<button className='submit-button' type='submit' placeholder='Input Here'>Submit</button>
-			</form>
-			<button
-				className='back-monitor'
-				onClick={() => {
-					setSelectedMonitor(4);
-					setIsHome(false);
-					setIsPupils(true);
-				}}
-			>
-				BACK
-			</button>
-		</main>
-	);
+  return (
+    <main className='Monitor'>
+      <section className='instructions'>
+        <ul className='instruction-steps'>
+          <p>Please input any extra information or notes here.</p>
+        </ul>
+      </section>
+      <form>
+
+        <input type='text' className='extraNotes-text' />
+        <button className='submit-button' type='submit' placeholder='Input Here'>Submit</button>
+      </form>
+      <button
+        className='back-monitor'
+        onClick={() => {
+          setSelectedMonitor(4);
+          setIsHome(false);
+          setIsPupils(true);
+        }}
+      >
+        BACK
+      </button>
+    </main>
+  );
 }
+
