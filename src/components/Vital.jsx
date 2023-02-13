86bfe2d9eac720db3da37d36578c009488070210
Wesley Matthee <wrmatthee@gmail.com>
2023-02-13 16:14:24 -0500

    Add vitals-icons

--- src/components/Vital.jsx
+++ src/components/Vital.jsx
@@ -1,24 +1,54 @@
-import React from "react";
-import "./Vital.css";
+import React from 'react';
+import './Vital.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { faCalendarDays } from '@fortawesome/free-solid-svg-icons';
+import { faHeartPulse } from '@fortawesome/free-solid-svg-icons';
+import { faLungs } from '@fortawesome/free-solid-svg-icons';
+import { faTemperatureThreeQuarters } from '@fortawesome/free-solid-svg-icons';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
+import { faPen } from '@fortawesome/free-solid-svg-icons';
 
-export default function Vital () {
+export default function Vital() {
+	return (
+		<>
+			<section className='vitals'>
+				<h2>CPR</h2>
+				<ul className='vitals-list'>
+					<p>
+						<FontAwesomeIcon icon={faLocationDot} className='vitals-icon' />
+						Location: Toronto
+					</p>
 
-
-  return (
-    <>
-        <section className='vitals'>
-        <h2>CPR</h2>
-        <ul className="vitals-list">
-            <p>Location: Toronto</p>
-            <p>Date: 2023-02-16</p>
-            <p>Pulse: 85/bpm</p>
-            <p>Respiration: 12/bpm</p>
-            <p>Skin: Warm and Dry</p>
-            <p>Pearl: True</p>
-            <p>Extra Notes: </p>
-            </ul>
-        </section>
-        </>
-
-  );
+					<p>
+						<FontAwesomeIcon icon={faCalendarDays} className='vitals-icon' />
+						Date: 2023-02-16
+					</p>
+					<p>
+						<FontAwesomeIcon icon={faHeartPulse} className='vitals-icon' />
+						Pulse: 85/bpm
+					</p>
+					<p>
+						<FontAwesomeIcon icon={faLungs} className='vitals-icon' />
+						Respiration: 12/bpm
+					</p>
+					<p>
+						<FontAwesomeIcon
+							icon={faTemperatureThreeQuarters}
+							className='vitals-icon'
+						/>
+						Skin: Warm and Dry
+					</p>
+					<p>
+						<FontAwesomeIcon icon={faEye} className='vitals-icon' />
+						Pearl: True
+					</p>
+					<p>
+						<FontAwesomeIcon icon={faPen} className='vitals-icon' />
+						Extra Notes:{' '}
+					</p>
+				</ul>
+			</section>
+		</>
+	);
 }
